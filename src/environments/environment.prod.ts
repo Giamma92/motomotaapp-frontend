bf8b59f5762853d69148952086c54cce902b1285
Gianmarco Moretti <moretti.gianmarco@gmail.com>
2025-02-23 00:21:09 +0100

    changed apiUrl vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://motomotaapp-backend.onrender.com/api'
+  apiUrl: 'https://motomotaapp-backend.vercel.app/api'
+  //apiUrl: 'https://motomotaapp-backend.onrender.com/api'
 };
